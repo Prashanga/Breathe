f29f6c88060b7d5f4f5195fe6483a21fa6b4ee4e
Prashanga <prashanga@gmail.com>
2020-07-18 21:13:09 +1000

    Preparing BoxBreathe component

---
+++ src/components/BoxBreathing/Index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="hello">
+    <h1>Hello, Box Breathing Here</h1>
+  </div>
+</template>
+
+<script>
+import "./style.scss";
+export default {
+  name: "BoxBreathing",
+  props: {
+    msg: String
+  }
+};
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,17 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div>
+    <BoxBreathing  />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import BoxBreathing from "@/components/BoxBreathing/Index.vue";
 
 export default {
   name: "Home",
   components: {
-    HelloWorld
+    BoxBreathing
   }
 };
 </script>
