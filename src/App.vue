938d4c2639a725a98cbeec17bd31e05c1d827958
Prashanga <prashanga@gmail.com>
2020-07-19 13:54:01 +1000

    Adding Appbar

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,15 @@
-<template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view />
-  </div>
-</template>
-
 <script>
 import './style.scss'
-export default {}
+import Navbar from '@/components/Navbar/Index.vue'
+
+export default {
+  components: { Navbar }
+}
 </script>
+
+<template>
+  <v-app>
+    <Navbar />
+    <router-view />
+  </v-app>
+</template>
---
+++ src/components/Navbar/Index.vue
@@ -0,0 +1,43 @@
+<template>
+  <v-app-bar absolute color="primary" dark elevate-on-scroll>
+    <v-app-bar-nav-icon></v-app-bar-nav-icon>
+
+    <v-toolbar-title>Title</v-toolbar-title>
+
+    <v-spacer></v-spacer>
+    <v-toolbar-items>
+      <v-btn
+        text
+        v-for="link in links"
+        :key="link.text"
+        router
+        :to="link.route"
+        exact
+      >
+        {{ link.text }}
+      </v-btn>
+    </v-toolbar-items>
+
+    <v-btn icon>
+      <v-icon>mdi-heart</v-icon>
+    </v-btn>
+  </v-app-bar>
+</template>
+
+<script>
+import './style.scss'
+
+export default {
+  name: 'Navbar',
+  data: () => {
+    return {
+      links: [
+        { text: 'Home', route: '/' },
+        { text: 'About', route: '#' }
+      ]
+    }
+  },
+  computed: {},
+  methods: {}
+}
+</script>
