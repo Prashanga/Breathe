ca9de95f79c19f1e28a82621413078c9af8d304d
Prashanga <prashanga@gmail.com>
2020-10-08 17:44:08 +1100

    Adding a 404 page

--- src/router/index.js
+++ src/router/index.js
@@ -18,6 +18,10 @@ const routes = [
     // which is lazy-loaded when the route is visited.
     component: () =>
       import(/* webpackChunkName: "about" */ '../views/About/Index.vue')
+  },
+  {
+    path: '*',
+    component: () => import('../views/NotFound.vue')
   }
 ]
 
---
+++ src/views/NotFound.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="NotFound">
+    <v-container>
+      <v-layout row wrap>
+        <v-flex xs12>
+          <v-card height="400" color="transparent" flat>
+            <div class="display-2 mt-10">
+              Page not found! <br />
+              404
+            </div>
+            <br />
+            <div class="paragraph-text mt-2">
+              <br />
+              You look lost. Please find your way back
+              <router-link to="/">Home </router-link>
+            </div>
+          </v-card>
+        </v-flex>
+      </v-layout>
+    </v-container>
+  </div>
+</template>
+
+<style>
+.paragraph-text {
+  font-size: 1.2rem;
+}
+a {
+  text-decoration: none;
+}
+</style>
